15891de29a49d3c9c2fb43fd58ec8bf926746724
Saadgitshub <leeunforgiven@gmail.com>
2024-07-06 18:41:11 +0100

    login/register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,36 +7,31 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['id'], message: 'There is already an account with this id')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface 
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180)]
     private ?string $username = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?bool $mfaEnabled = null;
-
+    /**
+     * @var list<string> The user roles
+     */
     #[ORM\Column]
-    private bool $isVerified = false;
+    private array $roles = [];
 
-    public function getRoles(): array
-    {
-        return array_unique(array_merge($this->roles, ['ROLE_USER']));
-    }
-
-    public function getUserIdentifier(): string
-    {
-        return $this->username;
-    }
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
@@ -55,7 +50,44 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -67,31 +99,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function isMfaEnabled(): ?bool
-    {
-        return $this->mfaEnabled;
-    }
-
-    public function setMfaEnabled(?bool $mfaEnabled): static
-    {
-        $this->mfaEnabled = $mfaEnabled;
-
-        return $this;
-    }
+    /**
+     * @see UserInterface
+     */
     public function eraseCredentials(): void
     {
-        // If you store any temporary sensitive data, clear it here
-    }
-
-    public function isVerified(): bool
-    {
-        return $this->isVerified;
-    }
-
-    public function setVerified(bool $isVerified): static
-    {
-        $this->isVerified = $isVerified;
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
