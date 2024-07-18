6ea5b798a2ce9850114d2cc9b4a6608bc117598e
Jetaofgak <75139221+Jetaofgak@users.noreply.github.com>
2024-07-18 23:09:03 +0100

    PLEASE AFTER THIS UPDATE DO NOT BUG ME

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -2,7 +2,7 @@
 
 
 namespace App\Command;
-/*
+
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -19,48 +19,9 @@ use Doctrine\ORM\EntityManagerInterface;
 )]
 class AddUserCommand extends Command
 {
-    protected static $defaultName = 'app:add-user';
-    private $entityManager;
-    private $passwordEncoder;
-
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        parent::__construct();
-
-        $this->entityManager = $entityManager;
-        $this->passwordEncoder = $passwordEncoder;
-    }
-
-    protected function configure()
-    {
-        $this
-            ->setDescription('Add a new user')
-            ->setHelp('This command allows you to add a new user');
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-
-        $username = $io->ask('Enter username');
-        $password = $io->askHidden('Enter password');
-        $mfaEnabled = $io->confirm('Enable MFA (Multi-Factor Authentication)?');
-
-        $user = new User();
-        $user->setUsername($username);
-        $encodedPassword = $this->passwordEncoder->encodePassword($user, $password);
-        $user->setPassword($encodedPassword);
-        $user->setMfaEnabled($mfaEnabled);
-
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
-
-        $io->success('User successfully added.');
-
-        return Command::SUCCESS;
-    }
+   
     
     
 
-}*/
+}
 ?>
