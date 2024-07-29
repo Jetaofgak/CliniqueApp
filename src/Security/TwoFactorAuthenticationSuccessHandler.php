15b131ddd3c89eb86927dba6e25eaf6a9ed0e88d
Saadgitshub <leeunforgiven@gmail.com>
2024-07-29 10:06:29 +0100

    final version

---
+++ src/Security/TwoFactorAuthenticationSuccessHandler.php
@@ -0,0 +1,17 @@
+<?php
+// src/Security/TwoFactorAuthenticationSuccessHandler.php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class TwoFactorAuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
+    {
+        // Return the response to tell the client that authentication, including two-factor authentication, is complete.
+        return new Response('{"login": "success", "two_factor_complete": true}');
+    }
+}
